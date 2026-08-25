9e7ed952d55526655b39741aa76d0fc21fd42432
ganeshuniyal08-collab <ganeshuniyal08@gmail.com>
2026-08-26 00:00:07 +0530

    Add files via upload

---
+++ q31.c
@@ -0,0 +1,28 @@
+//Q31: Write a program to take a number as input and print its equivalent binary representation.
+
+/*
+Sample Test Cases:
+Input 1:
+10
+Output 1:
+1010
+
+Input 2:
+7
+Output 2:
+111
+
+*/
+#include <stdio.h>
+int main(){
+    int a,b=0,c;
+    printf("enter an integer:");
+    scanf("%d", &a);
+    while (a!=0){
+        c=a%2;
+        b=b*10+c;
+        a/=2;
+
+    }
+    return 0;
+}
