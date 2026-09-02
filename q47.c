0ed4e2bb7c9a1c4f396123ce6e885409c4ffb8bd
ganeshuniyal08-collab <ganeshuniyal08@gmail.com>
2026-09-02 12:22:36 +0530

    Add files via upload

---
+++ q47.c
@@ -0,0 +1,29 @@
+/*Q47: Write a program to print the following pattern:
+*
+**
+***
+****
+*****
+
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+**
+***
+****
+*****
+
+*/
+#include <stdio.h>
+int main(){
+    int a,b;
+    for(a=1;a<=5;a++){
+        for(b=1;b<=a;b++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
