8dd9a6471517314917cd9bf41ba6a516918c74cb
ganeshuniyal08-collab <ganeshuniyal08@gmail.com>
2026-09-05 09:12:21 +0530

    Add files via upload

---
+++ q49.c
@@ -0,0 +1,30 @@
+/*Q49: Write a program to print the following pattern:
+5
+45
+345
+2345
+12345
+
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+5
+45
+345
+2345
+12345
+
+*/
+#include <stdio.h>
+int main(){
+  int a,b,c=5;
+  for (a=1;a<=5;a++){
+    for(b=1;b<=a;b++){
+      printf();
+    }
+    printf("\n");
+  }
+  return 0;
+}
